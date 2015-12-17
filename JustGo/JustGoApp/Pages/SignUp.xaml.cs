ce7f826b02bc8a0f34e2600d7c2c3f0716c9ec6b
iliantova <iliant.berk@gmail.com>
2015-12-17 22:54:38 +0200

    Sign Up with connection by Server

--- JustGo/JustGoApp/Pages/SignUp.xaml.cs
+++ JustGo/JustGoApp/Pages/SignUp.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -22,9 +23,27 @@ namespace JustGoApp.Pages
     /// </summary>
     public sealed partial class SignUp : Page
     {
+        private readonly HttpClient httpClient;
         public SignUp()
         {
             this.InitializeComponent();
+            this.httpClient = new HttpClient();
+        }
+        private async void OnRegisterUserButtonClick(object sender, RoutedEventArgs e)
+        {
+            var formContent = new FormUrlEncodedContent(new[]
+{
+    new KeyValuePair<string, string>("Username", userNameSignUp.Text),
+    new KeyValuePair<string, string>("Email", userNameSignUp.Text),
+    new KeyValuePair<string, string>("Password", passwordSignUp.Password),
+    new KeyValuePair<string, string>("ConfirmPassword", passwordConfirmSignUp.Password)
+});
+
+            var url = "http://localhost:15334/api/Account/Register";
+            var response = await httpClient.PostAsync(url, formContent);
+            var stringContent = await response.Content.ReadAsStringAsync();
+
+
         }
     }
 }
