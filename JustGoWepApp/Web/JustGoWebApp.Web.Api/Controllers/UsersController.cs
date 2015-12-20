f1a8ed4a89fc67671f1d45d2a0f551863eca9da5
Maskln <lyubomir.nanev@gmail.com>
2015-12-20 17:15:44 +0200

    Changed Get request in UsersController.cs

--- JustGoWepApp/Web/JustGoWebApp.Web.Api/Controllers/UsersController.cs
+++ JustGoWepApp/Web/JustGoWebApp.Web.Api/Controllers/UsersController.cs
@@ -1,13 +1,8 @@
 ﻿namespace JustGoWebApp.Web.Api.Controllers
 {
-    using System.ComponentModel.DataAnnotations;
     using System.Linq;
     using System.Web.Http;
-    using AutoMapper;
     using AutoMapper.QueryableExtensions;
-    using Data.Models;
-    using Infrastructure.Validation;
-    using Microsoft.AspNet.Identity;
     using Models.Users;
     using Services.Data.Contracts;
 
@@ -20,30 +15,17 @@
             this.users = users;
         }
 
-        public IHttpActionResult Get([Required]string id)
+        [Authorize]
+        public IHttpActionResult Get()
         {
+            string username = this.User.Identity.Name.ToString();
+
             var result = this.users
-                .GetByUserName(id)
-                .ProjectTo<UserResponseModel>()
+                .GetByUserName(username)
+                .ProjectTo<DataForUserResponseModel>()
                 .FirstOrDefault();
 
             return this.Ok(result);
         }
-
-        //[Authorize]
-        //[HttpPut]
-        //[Route("api/Users/Rate")]
-        //[ValidateModel]
-        //public IHttpActionResult Rate(RatingRequestModel model)
-        //{
-        //    if (this.User.Identity.GetUserId() == model.UserId)
-        //    {
-        //        return this.BadRequest("You cannot give rating to yourself!");
-        //    }
-
-        //    var rating = Mapper.Map<Rating>(model);
-        //    this.users.Rate(rating);
-        //    return this.Ok();
-        //}
     }
 }
